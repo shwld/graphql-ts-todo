3b73196d74a6cb80619aa0170ad7658633e897ee
shwld <shu.account@outlook.com>
2022-01-19 06:20:02 +0900

    taskデータを取得して画面に表示

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { AppProps } from 'next/app';
+import { withUrqlClient } from 'next-urql';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default withUrqlClient(
+  () => ({
+    url: 'http://localhost:3000/api/graphql',
+  }),
+  { ssr: false }
+)(MyApp);
