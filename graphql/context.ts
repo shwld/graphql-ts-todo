b9b4c17caf90bb82b350a49ef306700ee60a3386
shwld <shu.account@outlook.com>
2022-01-28 18:03:33 +0900

    export interfaceするとcontextの型がちゃんとついた

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from '@prisma/client';
+import { prisma } from '../prisma/db';
 
-export const context = {
-  db: new PrismaClient(),
+export interface Context {
+  db: PrismaClient;
+}
+
+export const context: { db: PrismaClient } = {
+  db: prisma,
 };
